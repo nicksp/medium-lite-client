2cc01daafcdd9d0e0b8c107f445d680a253c8eb2
Nick Plekhanov <nickatgit@yandex.com>
2017-05-11 02:50:25 +0300

    Set up the routing structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import store from './store';
 
 import App from './components/App';
+import Home from './components/Home';
+import Login from './components/Login'
 
 ReactDOM.render((
   <Provider store={store}>
-    <App />
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+        <Route path="login" component={Login} />
+      </Route>
+    </Router>
   </Provider>
 ), document.getElementById('root'));
