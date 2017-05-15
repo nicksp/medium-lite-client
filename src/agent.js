1610eba7886a553b6771564e66b703c091d04b61
Nick Plekhanov <nickatgit@yandex.com>
2017-05-15 15:50:03 +0300

    Wire up the login form

--- src/agent.js
+++ src/agent.js
@@ -5,13 +5,19 @@ const API_ROOT = 'https://api/server/path';
 const getResponseBody = response => response.body;
 
 const requests = {
-  get: url => superagent.get(`${API_ROOT}${url}`).then(getResponseBody)
+  get: url => superagent.get(`${API_ROOT}${url}`).then(getResponseBody),
+  post: (url, body) => superagent.post(`${API_ROOT}${url}`, body).then(getResponseBody)
 };
 
 const Articles = {
   all: page => requests.get('/articles?limit=10')
 };
 
+const Auth = {
+  login: (email, password) => requests.post('/users/login', { user: { email, password } })
+};
+
 export default {
-  Articles
+  Articles,
+  Auth
 };
---
+++ src/components/ListErrors.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class ListErrors extends Component {
+  render() {
+    const errors = this.props.errors;
+
+    if (errors) {
+      return (
+        <ul className="error-messages">
+          {Object.keys(errors).map(key => <li key={key}>{key} {errors[key]}</li>)}
+        </ul>
+      );
+    }
+  }
+}
