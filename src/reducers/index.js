d32b678621022e0fa29e9aa6298443a5e62d6699
Nick Plekhanov <nickatgit@yandex.com>
2017-06-07 15:03:36 +0300

    Finish profile view component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,20 @@
 import { combineReducers } from 'redux';
 
 import article from './article';
+import articleList from './articleList';
 import common from './common';
 import home from './home';
 import auth from './auth';
+import profile from './profile';
 import settings from './settings';
 
 const appReducer = combineReducers({
   article,
+  articleList,
   common,
   home,
   auth,
+  profile,
   settings
 });
 
